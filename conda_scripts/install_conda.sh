d0ac415517a7dd6c5e415ba96fe89e63d1d6343d
Ngoc Khuc Trong <ngoc.khuc@eastgate-software.com>
2023-09-24 21:48:31 +0700

    udpate EOL

--- conda_scripts/install_conda.sh
+++ conda_scripts/install_conda.sh
@@ -10,26 +10,25 @@ if [ ! -f "$MINICONDA_FILE" ]; then
     mkdir $TMP_FOLDER
     wget -O $MINICONDA_FILE https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
     echo $MINICONDA_FILE downloaded
+    
 else
     echo $MINICONDA_FILE exists
 fi
 
-
-if [ ! -d "$MINICONDA_FOLDER" ]; then 
+if [ ! -d "$MINICONDA_FOLDER" ]; then
     bash $MINICONDA_FILE -b -u -p ~/miniconda3
     ~/miniconda3/bin/conda init bash
 else
     echo Conda environment exists at $MINICONDA_FOLDER
 fi
 
-if { conda env list | grep "$ENV_NAME";} >/dev/null 2>&1; then
+if { conda env list | grep "$ENV_NAME"; } >/dev/null 2>&1; then
     echo Environtment $ENV_NAME exists
-    source /home/ngockhuc/miniconda3/bin/activate $ENV_NAME
+    source /$HOME/miniconda3/bin/activate $ENV_NAME
     which python
 else
-    echo Createing d
     conda create -y -n $ENV_NAME python=3.7
-    source /home/ngockhuc/miniconda3/bin/activate $ENV_NAME
+    source /$HOME/miniconda3/bin/activate $ENV_NAME
     which python
     pip install -r resources/requirements.txt
 fi
